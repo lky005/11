012224009e4908be293c3295b68d8318ddb8cb23
lky005 <3248076788@qq.com>
2025-12-19 00:25:05 +0800

    部署自动化编译工厂：添加Java接口、C++源码和GitHub脚本

---
+++ entry/src/main/cpp/native_lib.cpp
@@ -0,0 +1,10 @@
+﻿#include <jni.h>
+#include <string>
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_mylib_MyNativeLib_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from GitHub Actions!";
+    return env->NewStringUTF(hello.c_str());
+}
---
+++ entry/src/main/java/com/example/mylib/MyNativeLib.java
@@ -0,0 +1,11 @@
+﻿package com.example.mylib;
+
+public class MyNativeLib {
+    // 加载名为 "mylib" 的动态库
+    static {
+        System.loadLibrary("mylib");
+    }
+
+    // 声明 native 方法
+    public native String stringFromJNI();
+}
