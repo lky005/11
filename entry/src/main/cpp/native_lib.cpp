44f57ed54e411ccb341877d7def6246414ef2140
lky005 <3248076788@qq.com>
2025-12-19 00:52:26 +0800

    修复fatal error: 手动在CMake中添加JNI头文件路径

--- entry/src/main/cpp/native_lib.cpp
+++ entry/src/main/cpp/native_lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_mylib_MyNativeLib_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from HarmonyOS Native!";
+    std::string hello = "Hello from HarmonyOS (Compiled by GitHub Actions)!";
     return env->NewStringUTF(hello.c_str());
 }
